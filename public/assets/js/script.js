9076577d92dff3d0d82183a11c8906064aa9a793
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2022-11-30 02:30:36 -0500

    Money permanence

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -14,27 +14,36 @@ const updateServerMoney = (update) => {
       })
 }
 
-window.onload = () => {
-  console.log('Page has loaded.')
-  document.getElementById("newbill").addEventListener("submit", async (event) => {
-    event.preventDefault();
-    // let money = parseNumber(document.querySelector("#bank").innerHTML);
-    let money = await fetch("/api").then(response => response.json()).then((data) => data.money)
-    let spent = parseNumber(document.querySelector("#currency-field").value);
-    console.log(money, spent);
-    let newmoney = money - spent;
-    updateServerMoney(-spent)
-    if (newmoney < 0) {
-        document.querySelector("#bank").innerHTML = "-$" + Math.abs(newmoney);
+const getMoney = async () => {
+    return await fetch("/api").then(response => response.json()).then((data) => data.money)
+}
+
+const updateMoney = (update) => {
+    if (update < 0) {
+        document.querySelector("#bank").innerHTML = "-$" + Math.abs(update);
         document.querySelector("#bank").style.backgroundColor = "#C41E3A";
         document.querySelector("#bank").style.color = 'black';
     } else {
-        document.querySelector("#bank").innerHTML = "$" + newmoney;
+        document.querySelector("#bank").innerHTML = "$" + update;
         document.querySelector("#bank").style.backgroundColor = '#118C4F';
         document.querySelector("#bank").style.color = 'white';
     }
+}
 
-    document.querySelector("#currency-field").value = '';
-});
+window.onload = async () => {
+    let money = await getMoney();
+    updateMoney(money);
+    document.getElementById("newbill").addEventListener("submit", async (event) => {
+        event.preventDefault();
+        // let money = parseNumber(document.querySelector("#bank").innerHTML);
+        let money = await getMoney()
+        let spent = parseNumber(document.querySelector("#currency-field").value);
+        console.log(money, spent);
+        let newmoney = money - spent;
+        updateServerMoney(-spent)
+        updateMoney(newmoney)
+
+        document.querySelector("#currency-field").value = '';
+    });
 }
 
